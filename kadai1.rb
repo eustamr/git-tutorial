05746d7ac5b8611b9d42bb8380eb7126109ed5d6
matsue <j1828@matsue-ct.ac.jp>
2021-10-27 11:54:52 +0900

    ADD if

--- kadai1.rb
+++ kadai1.rb
@@ -1,3 +1,11 @@
 for i in 1..100 do
-    puts i
+    if i%15==0 then
+        puts "FizzBuzz"
+    elsif i%3==0 then
+        puts "Fizz"
+    elsif i%5==0 then
+        puts "Buzz"
+    else
+        puts i
+    end
 end
